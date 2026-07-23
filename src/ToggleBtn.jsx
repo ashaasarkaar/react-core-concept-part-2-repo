8439a429f277736ed0db175ec6757ad1737eeee2
ashaasarkaar <ashaasarkaar@gmail.com>
2026-07-23 16:02:14 +0600

    Questions Add For Hide Text

--- src/ToggleBtn.jsx
+++ src/ToggleBtn.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+/**
+ * 
+ * @returns 2. Task 2 : useState and conditional Rendering
+    
+    **Create a “show/hide Text” app:**
+    1. use useState to manage the visibility of a text message.
+    2. A button should toggle between “Show” and “Hide” text.
+    3. When the text is visible , it should say “Hello, React Leaner!”.
+    4. When hidden, nothing should be displayed.
+ */
+
 const ToggleBtn = () =>{
     const[text, setText] = useState(false)
 
